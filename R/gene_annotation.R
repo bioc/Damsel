4917d009b42ece0ba58b3679aa9eb1de57ec9351
Caitlin Page <caitlin.page@petermac.org>
2023-06-09 09:28:01 +1000

    initial commit - original functions for annotation

---
+++ R/gene_annotation.R
@@ -0,0 +1,21 @@
+extend_upstream <- function(granges, upstream=0) {
+  on_plus <- strand(granges) == "+"
+  new_start <- start(granges) - ifelse(on_plus, upstream, 0)
+  new_end <- end(granges) + ifelse(on_plus, 0, upstream)
+  start(granges) <- new_start
+  end(granges) <- new_end
+  granges
+}
+
+gene_annotate_fn <- function(granges, upstream, peaks) {
+  on_plus <- strand(granges) == "+"
+  new_start <- start(granges) - ifelse(on_plus, upstream, 0)
+  new_end <- end(granges) + ifelse(on_plus, 0, upstream)
+  start(granges) <- new_start
+  end(granges) <- new_end
+
+  plyranges::find_overlaps_within(plyranges::as_granges(mutate(peaks, start = peak_start, end = peak_end)),
+                       plyranges::as_granges(mutate(as.data.frame(granges), gene_width = width))) %>%
+    as.data.frame()
+
+}
