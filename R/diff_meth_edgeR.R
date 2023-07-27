cdbc11b0090f95d25d7b6aa8a04aeb67b29356c1
Caitlin Page <caitlin.page@petermac.org>
2023-07-28 09:16:59 +1000

    1st try roxygen doc for set_up fn

--- R/diff_meth_edgeR.R
+++ R/diff_meth_edgeR.R
@@ -1,4 +1,18 @@
 
+#' Differential methylation analysis
+#'
+#' `edgeR_set_up()` sets up the edgeR analysis for visualisation of the samples [edgeR_plot_mds()], and then for identifying differentially methylated regions [edgeR_results()].
+#'
+#'
+#' @param df Data frame generated from [process_bams]. Ensure that the samples are ordered by (Dam_1, Fusion_1, Dam_2, Fusion_2, ...)
+#' @param lib.size Library size for each sample is calculated as the sum across all rows for that sample unless otherwise specified
+#' @param keep_a Minimum cpm of the counts - default is 0.5
+#' @param keep_b Minimum number of samples to meet the criteria of keep_a in order to retain the region in the downstream analysis. Default is 3 (assuming 6 samples)
+#'
+#' @return Refer to [edgeR::?`DGEListClass`] for details
+#' @export
+#'
+#' @examples
 edgeR_set_up <- function(df, lib.size=NULL, keep_a=0.5, keep_b=3) {
   matrix <- as.matrix(df[,grepl("bam", colnames(df))]) # can I be sure they would have "bam" in it?
   rownames(matrix) <- df$Position
@@ -29,6 +43,15 @@ edgeR_set_up <- function(df, lib.size=NULL, keep_a=0.5, keep_b=3) {
   dge
 }
 
+#' Multidimensional scaling plot for samples
+#'
+#' this is a test for having 2 in 1 doc
+#' @param dge as outputted from [edgeR_set_up()]
+#'
+#' @return mds plot of samples -
+#' @export
+#'
+#' @examples
 edgeR_plot_mds <- function(dge) {
   group <- dge$samples$group %>% as.character()
   edgeR::plotMDS(dge, col=as.numeric(factor(group)))
@@ -41,7 +64,7 @@ edgeR_results <- function(dge, p.value=0.05, lfc=1) {
   qlf = edgeR::glmQLFTest(fit, coef = 2)
   summary(de <- edgeR::decideTestsDGE(qlf, p.value = p.value, lfc = lfc))
   lrt_table <- qlf$table
-  lrt_table$de <- as.data.frame(de)$groupFusion
+  lrt_table$de <- data.frame(de)$groupFusion
   write.table(lrt_table, file='../output/lrt_sd.txt', quote=F)
   write.table(keep, file='../output/keep', quote=F, col.names = FALSE)
   lrt_table
