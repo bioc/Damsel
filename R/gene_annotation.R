01884ba3d2de70daf11df65347b28bd0ee2d6161
Caitlin Page <caitlin.page@petermac.org>
2023-08-07 11:01:26 +1000

    added fn

--- R/gene_annotation.R
+++ R/gene_annotation.R
@@ -1,3 +1,27 @@
+
+get_biomart_genes <- function(species, version=109, regions) {
+  ensembl <- useEnsembl(biomart = "genes", dataset = species, version = version)
+  BM.info <- getBM(attributes=c("ensembl_gene_id", "ensembl_transcript_id", "transcript_is_canonical"),
+                   filters="chromosome_name",
+                   values= unique(regions$seqnames),
+                   mart=ensembl)
+  gene_features <- getBM(attributes = c("ensembl_gene_id", "external_gene_name", "ensembl_transcript_id", "chromosome_name", "start_position", "end_position", "strand", "transcription_start_site"),
+                         filters = "ensembl_transcript_id",
+                         values = filter(BM.info, transcript_is_canonical == 1)$ensembl_transcript_id,
+                         mart = ensembl)
+  gene_features <- gene_features %>% .[order(.$chromosome_name, .$start_position),]
+  colnames(gene_features) <- c("ensembl_gene_id", "gene_name", "ensembl_transcript_id", "seqnames", "start", "end", "strand", "TSS")
+  gene_features <- gene_features %>% as_granges() %>% data.frame()
+
+  overlap <- find_overlaps_within(as_granges(regions), as_granges(gene_features)) %>% data.frame() %>% group_by(ensembl_gene_id) %>% summarise(n_regions = n()) %>% data.frame()
+  gene_features$n_regions <- overlap[match(gene_features$ensembl_gene_id, overlap$ensembl_gene_id), "n_regions"]
+  gene_features <- gene_features %>% mutate(n_regions = coalesce(n_regions, 0), seqnames = paste0("chr", seqnames))
+
+  gene_features
+}
+
+
+
 extend_upstream <- function(granges, upstream=0) {
   on_plus <- strand(granges) == "+"
   new_start <- start(granges) - ifelse(on_plus, upstream, 0)
