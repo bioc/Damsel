db6a84ed63385661b31b78a17e0c3b338406834a
Caitlin Page <caitlin.page@petermac.org>
2023-08-15 16:22:31 +1000

    update

--- tests/testthat/test-process_bams.R
+++ tests/testthat/test-process_bams.R
@@ -3,7 +3,4 @@ test_that("Output is data frame", {
   expect_s3_class(process_bams(system.file("extdata", package = "Damsel")), "data.frame")
 })
 
-#failed - says that expect_contains doesn't exist
-#test_that("Df has expected column names", {
-#  expect_contains(colnames(process_bams(system.file("extdata", package = "Damsel"))), c("Position", "seqnames", "start", "end", "width", "strand"))
-#})
+
