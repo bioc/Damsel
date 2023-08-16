67c401f7bd6c70f93dc1131d079bb218641981b7
Caitlin Page <caitlin.page@petermac.org>
2023-08-16 15:17:28 +1000

    added tests for all new checks - if errors work, if no error

--- tests/testthat/test-diff_meth_edgeR.R
+++ tests/testthat/test-diff_meth_edgeR.R
@@ -1,17 +1,52 @@
 
-
+##edgeR set up
 test_that("Output is a DGEList", {
   expect_s4_class(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), "DGEList")
 })
 
+test_that("Output is error", {
+  expect_error(edgeR_set_up(), "Must have data.frame of counts", ignore.case = TRUE)
+  expect_error(edgeR_set_up(df = list(1,2,3)), "Must have data.frame of counts", ignore.case = TRUE)
+  expect_error(edgeR_set_up(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = parallel::detectCores()), keep_a = "A", keep_b = 3), "keep_a must be 1 value, recommend using default value", ignore.case = TRUE)
+  expect_error(edgeR_set_up(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = parallel::detectCores()), keep_a = 0.5, keep_b = c(1,2)), "keep_b must be 1 value, recommend using default value", ignore.case = TRUE)
+})
+
+
+test_that("Output is no error/message", {
+  expect_no_error(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = parallel::detectCores())))
+})
+
+##MDS plot
 test_that("Output is an MDS plot", {
   expect_s4_class(edgeR_plot_mds(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))), "MDS")
 })
 
+test_that("Output is error", {
+  expect_error(edgeR_plot_mds(), "requires DGE as outputted from `edgeR_set_up", ignore.case = TRUE)
+})
+
+test_that("Output is no error", {
+  expect_no_error(edgeR_plot_mds(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))))
+})
+
+##EdgeR results
 test_that("Output is a data frame", {
   expect_s3_class(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))), "data.frame")
 })
 
-test_that("Output is an MA plot", {
-  expect_s4_class(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), "MA")
+test_that("Output is error", {
+  expect_error(edgeR_results(), "requires DGE as outputted from `edgeR_set_up", ignore.case = TRUE)
+  expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), p.value = "A"), "p.value must be 1 number, recommend using default value", ignore.case = TRUE)
+  expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), p.value = c(1,2)), "p.value must be 1 number, recommend using default value", ignore.case = TRUE)
+  expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), lfc = "%"), "lfc must be 1 number, recommend using default value", ignore.case = TRUE)
+  expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), lfc = list(a = c("$", 2, "A"))), "lfc must be 1 number, recommend using default value", ignore.case = TRUE)
 })
+
+test_that("Output is no error", {
+  expect_no_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))))
+})
+
+##results plot - fn currently doesn't work
+#test_that("Output is an MA plot", {
+ # expect_s4_class(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), "MA")
+#})
--- tests/testthat/test-gene_annotation.R
+++ tests/testthat/test-gene_annotation.R
@@ -1,12 +1,45 @@
-
+#get genes
 test_that("Output is a data frame", {
   expect_s3_class(get_biomart_genes(species = "dmelanogaster_gene_ensembl"), "data.frame")
 })
 
+test_that("Output is error", {
+  expect_error(get_biomart_genes(), "Species must be a character vector", ignore.case = TRUE)
+  expect_error(get_biomart_genes(species = 2), "Species must be a character vector", ignore.case = TRUE)
+  expect_error(get_biomart_genes(species = "dmelanogaster_gene_ensembl", regions = matrix(2, nrow=3,ncol=5)), "Regions must be a data frame", ignore.case = TRUE)
+})
+
+test_that("Output is no error", {
+  expect_no_error(get_biomart_genes(species = "dmelanogaster_gene_ensembl"))
+})
+
+#annotate genes
 test_that("Output is a data.frame", {
   expect_s3_class(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), get_biomart_genes(species = "dmelanogaster_gene_ensembl")), "data.frame")
 })
 
+test_that("Output is error", {
+  expect_error(gene_annotate(genes = get_biomart_genes(species = "dmelanogaster_gene_ensembl")), "Require data.frame of peaks as outputted from `aggregate_peaks", ignore.case = TRUE)
+  expect_error(gene_annotate(peaks = list(a=c(2,3,4)), genes = get_biomart_genes(species = "dmelanogaster_gene_ensembl")), "Require data.frame of peaks as outputted from `aggregate_peaks", ignore.case = TRUE)
+  expect_error(gene_annotate(peaks = aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))))), "Requires data.frame of genes as outputted from `get_biomart_genes", ignore.case = TRUE)
+  expect_error(gene_annotate(peaks = aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), genes = 4), "Requires data.frame of genes as outputted from `get_biomart_genes", ignore.case = TRUE)
+})
+
+test_that("Output is no error", {
+  expect_no_error(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), get_biomart_genes(species = "dmelanogaster_gene_ensembl")))
+})
+
+
+#organised annotation
 test_that("Output is a data.frame", {
   expect_s3_class(gene_annotate_organised(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), get_biomart_genes(species = "dmelanogaster_gene_ensembl"))), "data.frame")
+})
+
+test_that("Output is error", {
+  expect_error(gene_annotate_organised(), "Requires data.frame of annotated peaks as outputted from `gene_annotate", ignore.case = TRUE)
+  expect_error(gene_annotate_organised(annotated_peaks = c("&", 2, "G")), "Requires data.frame of annotated peaks as outputted from `gene_annotate", ignore.case = TRUE)
+})
+
+test_that("Output is no error", {
+  expect_no_error(gene_annotate_organised(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))))), get_biomart_genes(species = "dmelanogaster_gene_ensembl"))))
 })
