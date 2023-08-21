0271f4c7a4596fb3fa3a645cb38a5e243f241baf
Caitlin Page <caitlin.page@petermac.org>
2023-08-21 10:50:05 +1000

    update - spelling

--- tests/testthat/test-correlation_results.R
+++ tests/testthat/test-correlation_results.R
@@ -37,6 +37,6 @@ test_that("Output is message", {
 
 test_that("Output is no error/message", {
   expect_no_error(corr_scatter(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = parallel::detectCores()), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = "Wing_Sd-1_S2_s.bam"))
-  expect_no_message(corr_heatmap(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = parallel::detectCores()), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = "Wing_Sd-1_S2_s.bam", method = "spearman"))
+  expect_no_message(corr_scatterp(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = parallel::detectCores()), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = "Wing_Sd-1_S2_s.bam", method = "spearman"))
 })
 
