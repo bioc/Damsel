aa7d1c0cc347ad2df064c959394618bfe84417aa
Caitlin Page <caitlin.page@petermac.org>
2023-08-21 10:51:06 +1000

    added the expected error message

--- tests/testthat/test-peak_calling.R
+++ tests/testthat/test-peak_calling.R
@@ -6,7 +6,7 @@ test_that("Output is a data.frame", {
 test_that("Output is error", {
   expect_error(add_de(), "Must have data frame of differential_testing results from `edgeR_results", ignore.case = TRUE)
   expect_error(add_de(de_results = matrix(3,5)), "Must have data frame of differential_testing results from `edgeR_results", ignore.case = TRUE)
-  expect_error(add_de(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))), regions = "A"))
+  expect_error(add_de(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))), regions = "A"), "Regions must be a data.frame", ignore.case = TRUE)
 })
 
 test_that("Output is not error", {
