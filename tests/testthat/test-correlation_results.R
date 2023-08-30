1085a2b7d4025d9ac4cebf11d6a063391ab40698
Caitlin Page <caitlin.page@petermac.org>
2023-08-30 10:42:51 +1000

    removed corr_scatter tests because removed the fn

--- tests/testthat/test-correlation_results.R
+++ tests/testthat/test-correlation_results.R
@@ -18,25 +18,5 @@ test_that("corr_heat:Output is no error/message", {
 })
 
 #scatterplot tests
-test_that("corr_scatter:Output is a plot", {
-  expect_s3_class(corr_scatter(process_bams(system.file("extdata", package = "Damsel")), "Wing_Dam-1_S1_s.bam", "Wing_Sd-1_S2_s.bam"), "gg")
-})
-
-test_that("corr_scatter:Output is error", {
-  expect_error(corr_scatter())
-  expect_error(corr_scatter(df = c(1,2,3)), "data.frame of counts is required")#, ignore.case = TRUE)
-  expect_error(corr_scatter(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_1 = "Wing_Dam-1_S1_s.bam"))#, ignore.case = TRUE)
-  expect_error(corr_scatter(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = c(1,2,3)), "sample_2 must be a character vector")#, ignore.case = TRUE)
-  expect_error(corr_scatter(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_2 = "Wing_Dam-1_S1_s.bam"))#, ignore.case = TRUE)
-  expect_error(corr_scatter(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_1 = list(c(1,2,3)), sample_2 = "Wing_Dam-1_S1_s.bam"), "sample_1 must be a character vector")#, ignore.case = TRUE)
-})
-
-test_that("corr_scatter:Output is message", {
-  expect_message(corr_scatter(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = "Wing_Sd-1_S2_s.bam"), "default spearman's method is used")#, ignore.case = TRUE)
-})
-
-test_that("corr_scatter:Output is no error/message", {
-  expect_no_error(corr_scatter(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = "Wing_Sd-1_S2_s.bam"))
-  expect_no_message(corr_scatter(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), sample_1 = "Wing_Dam-1_S1_s.bam", sample_2 = "Wing_Sd-1_S2_s.bam", method = "spearman"))
-})
+#removed because removing fn
 
