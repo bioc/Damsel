773d037d0c991ad5d9cbe2dff283f983b8920d10
Caitlin Page <caitlin.page@petermac.org>
2023-09-07 18:16:14 +1000

    update test layout

--- tests/testthat/test-correlation_results.R
+++ tests/testthat/test-correlation_results.R
@@ -1,18 +1,18 @@
 #heatmap tests
-test_that("corr_heatLOutput is a plot", {
+test_that("corr_heat: Output is a plot", {
   expect_s3_class(corr_heatmap(process_bams(system.file("extdata", package = "Damsel"))), "gg")
 })
 
-test_that("corr_heat:Output is error", {
+test_that("corr_heat: Output is error", {
   expect_error(corr_heatmap())
   expect_error(corr_heatmap(df = c(1,2,3)), "data.frame of counts is required")#, ignore.case = TRUE)
 })
 
-test_that("corr_heat:Output is message", {
+test_that("corr_heat: Output is message", {
   expect_message(corr_heatmap(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1)), "default spearman's method is used")#, ignore.case = TRUE)
 })
 
-test_that("corr_heat:Output is no error/message", {
+test_that("corr_heat: Output is no error/message", {
   expect_no_error(corr_heatmap(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1)))
   expect_no_message(corr_heatmap(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), method = "spearman"))
 })
--- tests/testthat/test-diff_meth_edgeR.R
+++ tests/testthat/test-diff_meth_edgeR.R
@@ -1,10 +1,10 @@
 
 ##edgeR set up
-test_that("edgeR set up:Output is a DGEList", {
+test_that("edgeR set up: Output is a DGEList", {
   expect_s4_class(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))), "DGEList")
 })
 
-test_that("edgeR set up:Output is error", {
+test_that("edgeR set up: Output is error", {
   expect_error(edgeR_set_up())
   expect_error(edgeR_set_up(df = list(1,2,3)), "Must have data.frame of counts")#, ignore.case = TRUE)
   expect_error(edgeR_set_up(df = process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1), keep_a = "A", keep_b = 3), "keep_a must be 1 value, recommend using default value")#, ignore.case = TRUE)
@@ -12,29 +12,29 @@ test_that("edgeR set up:Output is error", {
 })
 
 
-test_that("edgeR set up:Output is no error/message", {
+test_that("edgeR set up: Output is no error/message", {
   expect_no_error(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 1)))
 })
 
 ##MDS plot
-test_that("edgeR mds:Output is an MDS plot", {
+test_that("edgeR mds: Output is an MDS plot", {
   expect_s4_class(edgeR_plot_mds(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))), "MDS")
 })
 
-test_that("edgeR mds:Output is error", {
+test_that("edgeR mds: Output is error", {
   expect_error(edgeR_plot_mds())
 })
 
-test_that("edgeR mds:Output is no error", {
+test_that("edgeR mds: Output is no error", {
   expect_no_error(edgeR_plot_mds(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)])))
 })
 
 ##EdgeR results
-test_that("edgeR res:Output is a data frame", {
+test_that("edgeR res: Output is a data frame", {
   expect_s3_class(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)])), "data.frame")
 })
 
-test_that("edgeR res:Output is error", {
+test_that("edgeR res: Output is error", {
   expect_error(edgeR_results())
   expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]), p.value = "A"), "p.value must be 1 number, recommend using default value")#, ignore.case = TRUE)
   expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]), p.value = c(1,2)), "p.value must be 1 number, recommend using default value")#, ignore.case = TRUE)
@@ -42,7 +42,7 @@ test_that("edgeR res:Output is error", {
   expect_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]), lfc = list(a = c("$", 2, "A"))), "lfc must be 1 number, recommend using default value")#, ignore.case = TRUE)
 })
 
-test_that("edgeR res:Output is no error", {
+test_that("edgeR res: Output is no error", {
   expect_no_error(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)])))
 })
 
@@ -53,7 +53,7 @@ test_that("edgeR res:Output is no error", {
   #expect_s3_class(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel")))), "list")
 #})
 
-test_that("edgeR res plot:Output is error", {
+test_that("edgeR res plot: Output is error", {
   expect_error(edgeR_results_plot())
   expect_error(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]), p.value = list(c(1,2,3))), "p.value must be 1 number, recommend using default value")#, ignore.case = TRUE)
   expect_error(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]), p.value = "A"), "p.value must be 1 number, recommend using default value")#, ignore.case = TRUE)
@@ -61,6 +61,6 @@ test_that("edgeR res plot:Output is error", {
   expect_error(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]), lfc = data.frame(a = c("$", 2, "A"))), "lfc must be 1 number, recommend using default value")#, ignore.case = TRUE)
 })
 
-test_that("edgeR res plot:Output is no error", {
+test_that("edgeR res plot: Output is no error", {
   expect_no_error(edgeR_results_plot(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)])))
 })
