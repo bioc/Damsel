221be7c981075427e8521c84c5d8532cd4b4b1e3
Caitlin Page <caitlin.page@petermac.org>
2023-09-15 18:26:19 +1000

    Crucial fix: chromosome name, added stop for if 0 regions selected. Removed export for geom and updated how it accesses data

--- R/plot_region_counts.R
+++ R/plot_region_counts.R
@@ -51,7 +51,10 @@ plot_counts_all_bams <- function(df, seqnames, start_region = NULL, end_region =
   }
   df <- df
   colnames(df) <- chartr("-", "_", colnames(df))
-  df <- df %>% dplyr::filter(seqnames == seqnames, start >= start_region, end <= end_region)
+  df <- df %>% filter(seqnames == {{seqnames}}) %>% filter(start >= start_region, end <= end_region)
+  if(nrow(df) == 0) {
+    stop("No data available for provided region, make the region larger")
+  }
   df <- df %>% dplyr::mutate(number = 1:dplyr::n()) %>%
       .[rep(seq_len(nrow(.)), times = 4),] %>%
       .[order(.$number),] %>%
@@ -97,26 +100,21 @@ geom_regions.counts <- function(region.df = NULL, n_col = 1, region.color = "bla
 }
 
 
-#' @export
+
 ggplot_add.regions.counts <- function(object, plot, object_name) {
   # get plot data
-  # get plot data, plot data should contain bins
-  if (("patchwork" %in% class(plot)) && length(plot[[1]]$layers) == 1) {
-    plot.data <- plot[[1]]$layers[[1]]$data
-  } else if ("patchwork" %in% class(plot) && length(plot[[1]]$layers) == 2) {
-    plot.data <- plot[[1]]$layers[[2]]$data
-    colnames(plot.data) <- c("start", "end", "y1", "y2", "seqnames")
-  } else if (!("patchwork" %in% class(plot)) && length(plot$layers) == 1) {
-    plot.data <- plot$layers[[1]]$data
-  } else if (!("patchwork" %in% class(plot)) && length(plot$layers) == 2) {
-    plot.data <- plot$layers[[2]]$data
-    colnames(plot.data) <- c("start", "end", "y1", "y2", "seqnames")
+  plot2 <- plot
+  while("patchwork" %in% class(plot2)) {
+    plot2 <- plot2[[1]]
   }
+  plot.data <- plot2$labels$title
+  plot.data <- stringr::str_split_1(plot.data, ":")
   # prepare plot range
   # the plot region are not normal, so start is minimum value
-  plot.chr <- as.character(plot.data[1, "seqnames"])
-  plot.region.start <- min(plot.data[, "start"])
-  plot.region.end <- max(plot.data[, "end"])
+  plot.chr <- plot.data[1]
+  plot.data <- stringr::str_split_1(plot.data[2], "-")
+  plot.region.start <- plot.data[1] %>% as.numeric()
+  plot.region.end <- plot.data[2] %>% as.numeric()
 
   # get parameters
   region.df <- object$region.df
@@ -127,7 +125,7 @@ ggplot_add.regions.counts <- function(object, plot, object_name) {
 
   df <- region.df
   colnames(df) <- chartr("-", "_", colnames(df))
-  df <- df %>% dplyr::filter(seqnames == seqnames, start >= start_region, end <= end_region)
+  df <- df %>% dplyr::filter(seqnames == plot.chr, start >= plot.region.start, end <= plot.region.end)
   df <- df %>% dplyr::mutate(number = 1:dplyr::n()) %>%
     .[rep(seq_len(nrow(.)), times = 4),] %>%
     .[order(.$number),] %>%
