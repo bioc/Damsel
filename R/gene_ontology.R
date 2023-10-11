e35854450a23245eec9bfd66b93590017dc408e0
Caitlin Page <caitlin.page@petermac.org>
2023-10-12 10:41:04 +1100

    new goseq fn

--- R/gene_ontology.R
+++ R/gene_ontology.R
@@ -2,47 +2,53 @@
 #'
 #'  `goseq_fn` identifies the top 10 over-represented GO terms from the peak data, correcting for the number of GATC regions matching to each gene.
 #'
-#' @param regions data frame of regions between GATC sites - load data
+#' @param annotation data frame of annotated genes and peaks as `annotate_peaks_new()$all`
 #' @param genes data frame of gene data
-#' @param peaks data frame of annotated peak data
-#'
+#' @param regions data frame of regions between GATC sites - load data
+#' @param extend_by extend the start and end of the gene by default: 2000 bp.
+#' This is done to incorporate the acceptable distance of a peak to a gene.
+#' This also allows for consistency across significant and non-significant genes
+#' @param bias alternatively, the bias can be input by itself.
+#' @export
 #' @return 4 objects
 #'  * List of top 10 over-represented GO terms across the 3 GO categories
 #'  * Plot of goodness of fit of model
 #'  * Plot of sample data
 #'  * Plot of sample data without bias correction (should be messy)
-
 #geneOntology?
-goseq_fn <- function(regions, genes, peaks) {
-  goseq_genes_peaks <- plyranges::find_overlaps_within(plyranges::as_granges(regions), plyranges::as_granges(genes)) %>%
-      data.frame() %>%
-      dplyr::group_by(ensembl_gene_id) %>%
-      dplyr::mutate(n = dplyr::n()) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(de = ifelse(ensembl_gene_id %in% peaks$ensembl_gene_id, 1, 0)) %>%
-      dplyr::distinct(ensembl_gene_id, gene_width, n, de)
-  gene.vector <- goseq_genes_peaks$de
-  names(gene.vector) <- goseq_genes_peaks$ensembl_gene_id
+goseq_fn <- function(annotation, genes, regions, extend_by=2000, bias=NULL) {
+  de_genes <- dplyr::filter(annotation, min_distance <= extend_by)
+  goseq_data <- genes
+  goseq_data <- gene_mod_extend(goseq_data, regions, extend_by = {{extend_by}})
+  goseq_data <- goseq_data %>%
+    dplyr::mutate(de = ifelse(ensembl_gene_id %in% de_genes$ensembl_gene_id, 1, 0))
 
-  pwf <- goseq::nullp(gene.vector, "dm6", "ensGene", bias.data = goseq_genes_peaks$n)
+  gene.vector <- goseq_data$de
+  names(gene.vector) <- goseq_data$ensembl_gene_id
+
+  if(is.null(bias)) {
+    message("Bias will be n_regions that are contained within the gene length")
+    bias <- goseq_data$n_regions
+  }
+  pwf <- goseq::nullp(gene.vector, "dm6", "ensGene", bias.data = bias)
 
   GO.wall <- goseq::goseq(pwf, "dm6", "ensGene")
 
-  GO.samp <- goseq::goseq(pwf,"dm6","ensGene",method="Sampling",repcnt=1000)
+  GO.samp <- goseq::goseq(pwf, "dm6", "ensGene", method="Sampling", repcnt=1000)
 
   plot1 <- plot(log10(GO.wall[,2]), log10(GO.samp[match(GO.wall[,1],GO.samp[,1]),2]),
-               xlab="log10(Wallenius p-values)",ylab="log10(Sampling p-values)",
-               xlim=c(-3,0))
+                xlab="log10(Wallenius p-values)", ylab="log10(Sampling p-values)",
+                xlim=c(-3,0))
   abline(0,1,col=3,lty=2)
 
-  GO.nobias <- goseq::goseq(pwf,"dm6","ensGene",method="Hypergeometric")
+  GO.nobias <- goseq::goseq(pwf, "dm6", "ensGene", method="Hypergeometric")
 
-  plot2 <- plot(log10(GO.wall[,2]), log10(GO.nobias[match(GO.wall[,1],GO.nobias[,1]),2]),
-               xlab="log10(Wallenius p-values)", ylab="log10(Hypergeometric p-values)",
-               xlim=c(-3,0), ylim=c(-3,0))
-  abline(0,1,col=3,lty=2)
+  plot2 <- plot(log10(GO.wall[,2]), log10(GO.nobias[match(GO.wall[,1], GO.nobias[,1]), 2]),
+                xlab = "log10(Wallenius p-values)", ylab = "log10(Hypergeometric p-values)",
+                xlim = c(-3, 0), ylim = c(-3, 0))
+  abline(0, 1, col = 3, lty = 2)
 
-  enriched.GO <- GO.wall$category[stats::p.adjust(GO.wall$over_represented_pvalue, method="BH")<.05]
+  enriched.GO <- GO.wall$category[p.adjust(GO.wall$over_represented_pvalue, method = "BH")<.05]
   head(enriched.GO)
 
   list <- list(for(go in enriched.GO[1:10]){
@@ -53,4 +59,25 @@ goseq_fn <- function(regions, genes, peaks) {
   list
 }
 
-
+gene_mod_extend <- function(genes, regions, extend_by=2000) {
+  genes_mod <- genes
+  genes_mod$start <- genes_mod$start - extend_by
+  genes_mod$end <- genes_mod$end + extend_by
+  genes_mod <- genes_mod[,!(colnames(genes_mod) %in% "width")]
+  genes_mod <- genes_mod %>%
+    plyranges::as_granges() %>%
+    data.frame()
+  regions_gr <- dplyr::mutate(regions, seqnames = paste0("chr", seqnames)) %>%
+    plyranges::as_granges()
+  new <- plyranges::find_overlaps_within(regions_gr,
+                                         plyranges::as_granges(genes_mod)) %>%
+    data.frame() %>%
+    dplyr::group_by(ensembl_gene_id) %>%
+    dplyr::mutate(n_regions = dplyr::n()) %>%
+    dplyr::ungroup() %>%
+    data.frame()
+  genes_mod$n_regions <- new[match(genes_mod$ensembl_gene_id, new$ensembl_gene_id), "n_regions"]
+  genes_mod <- genes_mod %>%
+    dplyr::mutate(n_regions = dplyr::coalesce(n_regions, 0))
+  genes_mod
+}
