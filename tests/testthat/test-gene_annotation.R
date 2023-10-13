640693a724c76e58bc5301a3febfd719366e28dc
Caitlin Page <caitlin.page@petermac.org>
2023-10-13 17:03:03 +1100

    update to tests - deleting tests for old fns

--- tests/testthat/test-gene_annotation.R
+++ tests/testthat/test-gene_annotation.R
@@ -20,38 +20,6 @@ test_that("get_genes: Output is no error", {
   expect_no_message(get_biomart_genes(species = "dmelanogaster_gene_ensembl", version = 109, regions = regions_gatc_drosophila_dm6))
 })
 
-#annotate genes
-test_that("annotate: Output is a data.frame", {
-  expect_s3_class(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]))), get_biomart_genes(species = "dmelanogaster_gene_ensembl")), "data.frame")
-})
-
-test_that("annotate: Output is error", {
-  expect_error(gene_annotate())
-  expect_error(gene_annotate(genes = readRDS(test_path("fixtures", "test_genes.rds"))))
-  expect_error(gene_annotate(peaks = list(a=c(2,3,4)), genes = readRDS(test_path("fixtures", "test_genes.rds"))), "Require data.frame of peaks as outputted from `aggregate_peaks")#, ignore.case = TRUE)
-  expect_error(gene_annotate(peaks = readRDS(test_path("fixtures", "test_peaks.rds"))))
-  expect_error(gene_annotate(peaks = readRDS(test_path("fixtures", "test_peaks.rds")), genes = 4), "Requires data.frame of genes as outputted from `get_biomart_genes")#, ignore.case = TRUE)
-})
-
-test_that("annotate: Output is no error", {
-  expect_no_error(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]))), get_biomart_genes(species = "dmelanogaster_gene_ensembl")))
-  expect_no_error(gene_annotate(readRDS(test_path("fixtures", "test_peaks.rds")), readRDS(test_path("fixtures", "test_genes.rds"))))
-})
-
-
-#organised annotation
-test_that("gene_org: Output is a data.frame", {
-  expect_s3_class(gene_annotate_organised(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]))), get_biomart_genes(species = "dmelanogaster_gene_ensembl"))), "data.frame")
-})
-
-test_that("gene_org: Output is error", {
-  expect_error(gene_annotate_organised())
-  expect_error(gene_annotate_organised(annotated_peaks = c("&", 2, "G")), "Requires data.frame of annotated peaks as outputted from `gene_annotate")#, ignore.case = TRUE)
-})
-
-test_that("gene_org: Output is no error", {
-  expect_no_error(gene_annotate_organised(gene_annotate(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]))), get_biomart_genes(species = "dmelanogaster_gene_ensembl"))))
-})
 
 #####new annotation fn
 
@@ -60,14 +28,14 @@ test_that("gene_org: Output is no error", {
 #})
 
 test_that("annotate_genes.new: Output is error", {
-  expect_error(annotate_genes_new())
-  expect_error(annotate_genes_new(genes = readRDS(test_path("fixtures", "test_genes.rds"))))
-  expect_error(annotate_genes_new(peaks = list(a=c(2,3,4)), genes = readRDS(test_path("fixtures", "test_genes.rds"))), "Require data.frame of peaks as outputted from `aggregate_peaks")#, ignore.case = TRUE)
-  expect_error(annotate_genes_new(peaks = readRDS(test_path("fixtures", "test_peaks.rds"))))
-  expect_error(annotate_genes_new(peaks = readRDS(test_path("fixtures", "test_peaks.rds")), genes = 4), "Requires data.frame of genes as outputted from `get_biomart_genes")#, ignore.case = TRUE)
+  expect_error(annotate_genes())
+  expect_error(annotate_genes(genes = readRDS(test_path("fixtures", "test_genes.rds"))))
+  expect_error(annotate_genes(peaks = list(a=c(2,3,4)), genes = readRDS(test_path("fixtures", "test_genes.rds"))), "Require data.frame of peaks as outputted from `aggregate_peaks")#, ignore.case = TRUE)
+  expect_error(annotate_genes(peaks = readRDS(test_path("fixtures", "test_peaks.rds"))))
+  expect_error(annotate_genes(peaks = readRDS(test_path("fixtures", "test_peaks.rds")), genes = 4), "Requires data.frame of genes as outputted from `get_biomart_genes")#, ignore.case = TRUE)
 })
 
 test_that("annotate_genes.new: Output is no error", {
-  expect_no_error(annotate_genes_new(readRDS(test_path("fixtures", "test_peaks_new.rds")), readRDS(test_path("fixtures", "test_genes.rds"))))
-  expect_no_error(annotate_genes_new(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]))), get_biomart_genes(species = "dmelanogaster_gene_ensembl"), regions = regions_gatc_drosophila_dm6))
+  expect_no_error(annotate_genes(readRDS(test_path("fixtures", "test_peaks_new.rds")), readRDS(test_path("fixtures", "test_genes.rds"))))
+  expect_no_error(annotate_genes(aggregate_peaks(edgeR_results(edgeR_set_up(process_bams(system.file("extdata", package = "Damsel"))[,c(1:6,7,10,8,11,9,12)]))), get_biomart_genes(species = "dmelanogaster_gene_ensembl"), regions = regions_gatc_drosophila_dm6))
 })
--- tests/testthat/test-gene_ontology.R
+++ tests/testthat/test-gene_ontology.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("gene_ontology: Output is error", {
+  expect_error(goseq_fn())
+  expect_error(goseq_fn(genes = readRDS(test_path("fixtures", "test_genes.rds"))))
+  expect_error(goseq_fn(peaks = list(a=c(2,3,4)), genes = readRDS(test_path("fixtures", "test_genes.rds"))))#, "Require data.frame of peaks as outputted from `aggregate_peaks")#, ignore.case = TRUE)
+  expect_error(goseq_fn(peaks = readRDS(test_path("fixtures", "test_peaks.rds"))))
+  expect_error(goseq_fn(peaks = readRDS(test_path("fixtures", "test_peaks.rds")), regions = 4))#, "Requires data.frame of genes as outputted from `get_biomart_genes")#, ignore.case = TRUE)
+})
+
+test_that("gene_ontology: Output is no error", {
+  expect_no_error(goseq_fn(annotate_genes_new(readRDS(test_path("fixtures", "test_peaks_new.rds")), readRDS(test_path("fixtures", "test_genes.rds")), regions_gatc_drosophila_dm6)$all, readRDS(test_path("fixtures", "test_genes.rds")), regions_gatc_drosophila_dm6))
+  #expect_no_error(goseq_fn(annotate_genes_new(aggregate_peaks(readRDS(test_path("fixtures", "test_results.rds")), regions_gatc_drosophila_dm6), readRDS(test_path("fixtures", "test_genes.rds")), regions = regions_gatc_drosophila_dm6)$all, readRDS(test_path("fixtures", "test_genes.rds")), regions_gatc_drosophila_dm6))
 })
