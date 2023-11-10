99834e0af5dc2eec2f82111037cdc55ca3bc23b7
Caitlin Page <caitlin.page@petermac.org>
2023-11-10 12:59:49 +1100

    added option for plot format and changed default number of columns

--- R/plot_region_counts.R
+++ R/plot_region_counts.R
@@ -7,7 +7,8 @@
 #' @param seqnames A character string of the chromosome of interest
 #' @param start_region A number providing the start of region to plot
 #' @param end_region A number providing the end of region to plot
-#' @param n_col The number of columns to facet the graph by. Default is 3
+#' @param n_col The number of columns to facet the graph by. Default is 1
+#' @param layout Determines the layout of the plot. Default is "stacked" collapsing the Dam samples into one plot, and the Fusion samples into another. Samples can be plotted separately using "spread".
 #'
 #' @return A `ggplot` object.
 #' @export
@@ -19,7 +20,14 @@
 #'                      seqnames = "chr2L",
 #'                      start_region = 1,
 #'                      end_region = 40000,
-#'                      n_col = 3)
+#'                      n_col = 1,
+#'                      layout = "stacked")
+#' plot_counts_all_bams(counts.df,
+#'                      seqnames = "chr2L",
+#'                      start_region = 1,
+#'                      end_region = 40000,
+#'                      n_col = 1,
+#'                      layout = "spread")
 #' # Can use this plot to layer other plots -----------------------------
 #' dm_results <- random_edgeR_results()
 #' plot_counts_all_bams(counts.df,
@@ -28,7 +36,7 @@
 #'                      end_region = 40000,
 #'                      n_col = 1) +
 #'   geom_dm.res.lfc(dm_results)
-plot_counts_all_bams <- function(counts.df, seqnames, start_region = NULL, end_region = NULL, n_col = 3) {
+plot_counts_all_bams <- function(counts.df, seqnames, start_region = NULL, end_region = NULL, n_col = 1, layout = c("stacked", "spread")) {
   if(!is.data.frame(counts.df)) {
     stop("data.frame of counts is required")
   }
@@ -67,77 +75,32 @@ plot_counts_all_bams <- function(counts.df, seqnames, start_region = NULL, end_r
   df <- df %>% tidyr::gather(key = "bam",
                              value = "raw_counts",
                              colnames(.[, grepl(".bam", names(.))]))
-  df %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_polygon(ggplot2::aes(x = .data$Position, y = .data$raw_counts)) +
-    ggplot2::scale_x_continuous(expand = c(0,0)) +
-    ggplot2::coord_cartesian(xlim = c(start_region, end_region)) +
-    ggplot2::facet_wrap(~ bam, ncol = n_col) +
-    ggplot2::labs(title = paste0(seqnames, ":", start_region, "-", end_region))
-}
-
-
-
-geom_regions.counts <- function(region.df = NULL, n_col = 1, region.color = "black",
-                                plot.space = 0.1, plot.height = 1) {
-  structure(list(
-    region.df = region.df, n_col = n_col, region.color = region.color,
-    plot.space = plot.space, plot.height = plot.height
-  ),
-  class = "regions.counts"
-  )
-}
-
-
-
-ggplot_add.regions.counts <- function(object, plot, object_name) {
-  # get plot data
-  plot2 <- plot
-  while("patchwork" %in% class(plot2)) {
-    plot2 <- plot2[[1]]
+  if(length(layout) > 1) {
+    layout <- "stacked"
   }
-  plot.data <- plot2$labels$title
-  plot.data <- stringr::str_split_1(plot.data, ":")
-  # prepare plot range
-  # the plot region are not normal, so start is minimum value
-  plot.chr <- plot.data[1]
-  plot.data <- stringr::str_split_1(plot.data[2], "-")
-  plot.region.start <- plot.data[1] %>% as.numeric()
-  plot.region.end <- plot.data[2] %>% as.numeric()
-
-  # get parameters
-  region.df <- object$region.df
-  n_col <- object$n_col
-  region.color <- object$region.color
-  plot.space <- object$plot.space
-  plot.height <- object$plot.height
-
-  df <- region.df
-  colnames(df) <- chartr("-", "_", colnames(df))
-  df <- df %>% dplyr::filter(.data$seqnames == plot.chr, .data$start >= plot.region.start, .data$end <= plot.region.end)
-  df <- df %>% dplyr::mutate(number = 1:dplyr::n()) %>%
-    .[rep(seq_len(nrow(.)), times = 4),] %>%
-    .[order(.$number),] %>%
-    dplyr::group_by(.data$number) %>%
-    dplyr::mutate(num = 1:dplyr::n()) %>%
-    dplyr::mutate(Position = dplyr::case_when(.data$num == 1 ~ .data$start,
-                                              .data$num == 2 ~ .data$start,
-                                              .data$num == 3 ~ .data$end,
-                                              TRUE ~ .data$end))
-  df <- df %>% dplyr::mutate_at(ggplot2::vars(tidyr::matches(".bam")), ~ dplyr::case_when(.data$num == 1 ~ 0,
-                                                                                   .data$num == 2 ~ .,
-                                                                                   .data$num == 3 ~ .,
-                                                                                   TRUE ~ 0))
-  counts.plot <- df %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_polygon(ggplot2::aes(x = .data$Position, y = .data$raw_counts)) +
-    ggplot2::scale_x_continuous(expand = c(0,0)) +
-    ggplot2::coord_cartesian(xlim = c(plot.region.start, plot.region.end)) +
-    ggplot2::facet_wrap(~ bam, ncol = n_col) +
-    ggplot2::labs(title = paste0(seqnames, ":", plot.region.start, "-", plot.region.start))
-
-  patchwork::wrap_plots(plot + ggplot2::theme(plot.margin = ggplot2::margin(t = plot.space, b = plot.space)),
-                        counts.plot,
-                        ncol = 1, heights = c(1, plot.height)
-  )
+  if(layout == "stacked") {
+    df <- df %>%
+      dplyr::mutate(dam = ifelse(grepl("Dam", .data$bam), "Dam", "Fusion")) %>%
+      dplyr::group_by(.data$dam) %>%
+      dplyr::mutate(dam_num = as.integer(factor(.data$bam))) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(dam_2 = paste0(.data$dam, "_", .data$dam_num))
+    plot <- df %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_polygon(ggplot2::aes(x = .data$Position, y = .data$raw_counts, fill = as.factor(.data$dam_num)), alpha = 0.5) +
+      ggplot2::scale_x_continuous(expand = c(0,0)) +
+      ggplot2::coord_cartesian(xlim = c(start_region, end_region)) +
+      ggplot2::facet_wrap(~ .data$dam, ncol = n_col) +
+      ggplot2::labs(title = paste0(seqnames, ":", start_region, "-", end_region),
+                    fill = "Replicate")
+  } else if(layout == "spread") {
+    plot <- df %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_polygon(ggplot2::aes(x = .data$Position, y = .data$raw_counts)) +
+      ggplot2::scale_x_continuous(expand = c(0,0)) +
+      ggplot2::coord_cartesian(xlim = c(start_region, end_region)) +
+      ggplot2::facet_wrap(~ bam, ncol = n_col) +
+      ggplot2::labs(title = paste0(seqnames, ":", start_region, "-", end_region))
+  }
+  plot
 }
--- tests/testthat/test-plot_region_counts.R
+++ tests/testthat/test-plot_region_counts.R
@@ -19,4 +19,5 @@ test_that("plot counts all bams: output is error", {
 test_that("plot counts all bams: output is no error", {
   expect_no_error(plot_counts_all_bams(process_bams(system.file("extdata", package = "Damsel")), seqnames = "chr2L", start_region = 1, end_region = 10000))
   expect_no_error(plot_counts_all_bams(readRDS(test_path("fixtures", "test_counts_df.rds")), seqnames = "chr2L", start_region = 1, end_region = 10000))
+  expect_no_error(plot_counts_all_bams(readRDS(test_path("fixtures", "test_counts_df.rds")), seqnames = "chr2L", start_region = 1, end_region = 10000, layout = "spread"))
 })
