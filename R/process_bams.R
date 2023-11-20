22a14ae2bc9de7dc2011144b59d5993f639c6384
Caitlin Page <caitlin.page@petermac.org>
2023-11-21 09:34:17 +1100

    Major update, using featureCounts to get data instead of exomeCopy - believe more accurate and also allows for separation of paired end vs non paired end bams

--- R/process_bams.R
+++ R/process_bams.R
@@ -4,9 +4,10 @@
 #'
 #' @param path_to_bams A character string input vector. This must identify the directory containing the BAM files.
 #' @param regions A data.frame of GATC regions. If not specified, will use default data file `regions_gatc_drosophila_dm6`. The GATC regions can be made with `gatc_track()`.
-#' @param cores The number of computer cores to be used to parallelise the function and decrease it's run time. If not specified, will use default (2 cores).
+#' @param nthreads The number of computer cores to be used to parallelise the function and decrease it's run time. If not specified, will use default (2 cores).
 #' * If computer is being used for multiple tasks at once, we recommend reducing the number of cores - or leave it at the default setting.
 #' * The number of available cores can be checked using [parallel::detectCores()]
+#' @param ... Other arguments passed onto `Rsubread::featureCounts()`
 #'
 #' @return A `data.frame` containing the GATC region information in the form in the columns: seqnames (chromosome), start, end, width, and strand. The count information for the BAM files is in the subsequent columns, named by the name of the BAM file.
 #' * The ".bam" extension is retained in the sample name as an identifier for the sample columns
@@ -17,13 +18,13 @@
 #' path_to_bams <- system.file("extdata", package = "Damsel")
 #' counts.df <- process_bams(path_to_bams,
 #'                           regions = regions_gatc_drosophila_dm6,
-#'                           cores=2)
+#'                           nthreads=2)
 #' head(counts.df)
 #' # rearrange columns of bam files so that: Dam_1, Fusion_1, Dam_2, Fusion_2
 #' head(counts.df[,c(1:6,9,7,10,8)])
 #' @export
 #rename to processBams
-process_bams <- function(path_to_bams, regions=regions_gatc_drosophila_dm6, cores=2) {
+process_bams <- function(path_to_bams, regions=regions_gatc_drosophila_dm6, nthreads=2, ...) {
   if(!is.character(path_to_bams)) {
     stop("Path to bams must be a character vector")
   }
@@ -33,52 +34,63 @@ process_bams <- function(path_to_bams, regions=regions_gatc_drosophila_dm6, core
   if(missing(regions)) {
     message("regions missing, regions_gatc_drosophila_dm6 used instead")
   }
-  if(missing(cores)) {
-    message("cores missing, 2 cores used instead")
+  if(missing(nthreads)) {
+    message("nthreads missing, 2 cores used instead")
   }
 
-  #list of all bam files
-  files <- list.files(path_to_bams, pattern = ".bam")
+  regions_feat <- regions[,!names(regions) %in% "width"]
+  colnames(regions_feat) <- c("GeneID", "Chr", "Start", "End", "Strand")
+  regions_feat$Start <- as.integer(regions_feat$Start)
+  regions_feat$End <- as.integer(regions_feat$End)
+
+  list_files <- list.files(path_to_bams, pattern = ".bam")
   #check
-  if(length(files) == 0) {
+  if(length(list_files) == 0) {
     stop("No bam files present in path")
   }
-
-  files <- files %>%
-      data.frame() %>%
-      stats::setNames(., "file") %>%
-      dplyr::mutate(bai = stringr::str_detect(.data$file, "bai"))
-  if(S4Vectors::isEmpty(dplyr::filter(files, .data$bai == TRUE))) {
-    stop("No .bai files present in path")
+  if(length(list_files[grepl(".bai", list_files)]) == 0) {
+    stop("No .bai files present in path: Please ensure every BAM file has a corresponding .bai file")
   }
-  #want to add a check for unequal n of bams and bai
-
-    #remove any bai files
-  files <- files %>%
-      dplyr::filter(.data$bai == FALSE)
-  #add / to get path for each file
+  list_files <- list_files[!grepl(".bai", list_files)]
   path_to_bams <- ifelse(substring(path_to_bams, first = nchar(path_to_bams)) == "/", path_to_bams, paste0(path_to_bams, "/"))
-  files <- paste0(path_to_bams, files$file)
-  df <- regions
-  #test format of seqnames and separate them
-  names_chrom <- names(Rsamtools::scanBamHeader(files[1])[[1]][[1]])
-  same_name <- df$seqnames %in% names_chrom %>% unique()
-  if(same_name == TRUE) {
-    one_count <- parallel::mclapply(files, function(x) {exomeCopy::countBamInGRanges(x, plyranges::as_granges(df))}, mc.cores = cores) %>%
-      data.frame()
-    colnames(one_count) <- files
-  } else {
-    df$seqnames <- paste0("chr", df$seqnames)
-    one_count <- parallel::mclapply(files, function(x) {exomeCopy::countBamInGRanges(x, plyranges::as_granges(df))}, mc.cores = cores) %>%
-      data.frame()
-    colnames(one_count) <- files
+  list_files <- paste0(path_to_bams, list_files)
+
+  scan_result <- list()
+  for (i in 1:length(list_files)) {
+    scan_result <- list(scan_result, Rsamtools::testPairedEndBam(list_files[i]))
   }
+  scan_result <- unlist(scan_result)
+  if(length(unique(scan_result)) > 1) {
+    return(scan_result)
+  }
+  counts_feature <- regions
+  names_chrom <- names(Rsamtools::scanBamHeader(list_files[1])[[1]][[1]])
+  same_name <- counts_feature$seqnames %in% names_chrom %>% unique()
 
-  df2 <- cbind(df, one_count)
-  df2$seqnames <- paste0("chr", df2$seqnames)
-  colnames(df2) <- sub(path_to_bams, "", colnames(df2))
-  df2
-
+  if(same_name == TRUE) {
+    if(unique(unlist(scan_result)) == TRUE) {
+      for (i in 1:length(list_files)) {
+        counts_feature <- cbind(counts_feature, data.frame(Rsubread::featureCounts(list_files[i], annot.ext = regions_feat, isPairedEnd = TRUE, allowMultiOverlap = TRUE, fraction = TRUE, nthreads = nthreads, ...)$counts))
+      }
+    } else {
+      for (i in 1:length(list_files)) {
+        counts_feature <- cbind(counts_feature, data.frame(Rsubread::featureCounts(list_files[i], annot.ext = regions_feat, allowMultiOverlap = TRUE, fraction = TRUE, nthreads = nthreads, ...)$counts))
+      }
+    }
+    counts_feature$seqnames <- paste0("chr", counts_feature$seqnames)
+  } else {
+    counts_feature$seqnames <- paste0("chr", counts_feature$seqnames)
+    if(unique(unlist(scan_result)) == TRUE) {
+      for (i in 1:length(list_files)) {
+        counts_feature <- cbind(counts_feature, data.frame(Rsubread::featureCounts(list_files[i], annot.ext = regions_feat, isPairedEnd = TRUE, allowMultiOverlap = TRUE, fraction = TRUE, nthreads = nthreads, ...)$counts))
+      }
+    } else {
+      for (i in 1:length(list_files)) {
+        counts_feature <- cbind(counts_feature, data.frame(Rsubread::featureCounts(list_files[i], annot.ext = regions_feat, allowMultiOverlap = TRUE, fraction = TRUE, nthreads = nthreads, ...)$counts))
+      }
+    }
+  }
+  counts_feature
 }
 
 
--- tests/testthat/test-process_bams.R
+++ tests/testthat/test-process_bams.R
@@ -1,7 +1,8 @@
 
 test_that("Output is data frame", {
-  expect_s3_class(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 2), "data.frame")
+  expect_s3_class(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, nthreads = 2), "data.frame")
 })
+
 
 test_that("Output is error", {
   expect_error(process_bams(regions = regions_gatc_drosophila_dm6))
@@ -13,7 +14,7 @@ test_that("Output is error", {
 
 
 test_that("Output is a message", {
-  expect_message(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), cores = 2), "regions missing, regions_gatc_drosophila_dm6 used instead")#, ignore.case = TRUE)
+  expect_message(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), nthreads = 2), "regions missing, regions_gatc_drosophila_dm6 used instead")#, ignore.case = TRUE)
   #throws error for next one
   #expect_message(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6), "cores missing, [parallel::detectCores()] used instead", ignore.case = TRUE)
 })
@@ -21,5 +22,5 @@ test_that("Output is a message", {
 test_that("No error/warning/message", {
   expect_no_error(process_bams(path_to_bams = system.file("extdata", package = "Damsel")))
   #expect_no_warning(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6))
-  expect_no_message(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, cores = 2))
+  expect_no_message(process_bams(path_to_bams = system.file("extdata", package = "Damsel"), regions = regions_gatc_drosophila_dm6, nthreads = 2))
 })
