760c84c272c43b9cedad5762feb53ea7b261fe8d
Caitlin Page <caitlin.page@petermac.org>
2024-01-24 16:21:27 +1100

    ensure df - also indent of 4

--- R/diff_meth_edgeR.R
+++ R/diff_meth_edgeR.R
@@ -15,49 +15,47 @@
 #' counts.df <- random_counts()
 #'
 #' edgeR_set_up(counts.df)
-#dmSetUp
-edgeR_set_up <- function(counts.df, max.width=10000, lib.size=NULL, keep_a=0.5, keep_b=3) {
-  if(!is.data.frame(counts.df)) {
-    stop("Must have data.frame of counts")
-  }
-  if(!is.numeric(keep_a) | length(keep_a) > 1) {
-    stop("keep_a must be 1 value, recommend using default value")
-  }
-  if(!is.numeric(keep_b) | length(keep_b) > 1) {
-    stop("keep_b must be 1 value, recommend using default value")
-  }
+# dmSetUp
+edgeR_set_up <- function(counts.df, max.width = 10000, lib.size = NULL, keep_a = 0.5, keep_b = 3) {
+    if (!is.data.frame(counts.df)) {
+        stop("Must have data.frame of counts")
+    }
+    if (!is.numeric(keep_a) | length(keep_a) > 1) {
+        stop("keep_a must be 1 value, recommend using default value")
+    }
+    if (!is.numeric(keep_b) | length(keep_b) > 1) {
+        stop("keep_b must be 1 value, recommend using default value")
+    }
 
-  counts.df <- counts.df %>% dplyr::filter(.data$width <= max.width)
-  matrix <- as.matrix(counts.df[,grepl("bam", colnames(counts.df))]) # can I be sure they would have "bam" in it?
-  rownames(matrix) <- counts.df$Position
+    counts.df <- counts.df %>% dplyr::filter(.data$width <= max.width)
+    matrix <- as.matrix(counts.df[, grepl("bam", colnames(counts.df))]) # can I be sure they would have "bam" in it?
+    rownames(matrix) <- counts.df$Position
 
-  n_samples <- seq_len(ncol(matrix)/2)
+    n_samples <- seq_len(ncol(matrix) / 2)
 
-  group <- rep(c("Dam", "Fusion"), times = length(n_samples)) #specify that this order is required
+    group <- rep(c("Dam", "Fusion"), times = length(n_samples)) # specify that this order is required
 
-  dge <- edgeR::DGEList(matrix, lib.size = lib.size, group = group, gene = counts.df[,2:5])
+    dge <- edgeR::DGEList(matrix, lib.size = lib.size, group = group, gene = counts.df[, 2:5])
 
-  keep <- rowSums(edgeR::cpm(dge) >= keep_a) >= keep_b #potentially will mess with
-  dge <- dge[keep, , keep.lib.sizes = FALSE]
+    keep <- rowSums(edgeR::cpm(dge) >= keep_a) >= keep_b # potentially will mess with
+    dge <- dge[keep, , keep.lib.sizes = FALSE]
 
-  dge <- edgeR::calcNormFactors(dge)
+    dge <- edgeR::calcNormFactors(dge)
 
-  design_df <- seq_len(ncol(matrix)) %>%
-                 data.frame() %>%
-                 stats::setNames("group")
-  zero_vec <- rep(0, times = length(n_samples))
-  #need to replace the 1:length with seq along or something
-  for(i in n_samples) {
-    design <- replace(zero_vec, i, 1)
-    design <- rep(design, each = 2)
-    design_df[,ncol(design_df) + 1] <- design
+    design_df <- seq_len(ncol(matrix)) %>%
+        data.frame() %>%
+        stats::setNames("group")
+    zero_vec <- rep(0, times = length(n_samples))
+    for (i in n_samples) {
+        design <- replace(zero_vec, i, 1)
+        design <- rep(design, each = 2)
+        design_df[, ncol(design_df) + 1] <- design
+    }
+    design <- stats::model.matrix(~., data = design_df[, seq_len(ncol(design_df)) - 1])
 
-  }
-  design <- stats::model.matrix(~., data = design_df[, seq_len(ncol(design_df)) - 1])
+    dge <- edgeR::estimateDisp(dge, robust = TRUE, design = design)
 
-  dge <- edgeR::estimateDisp(dge, robust = TRUE, design = design)
-
-  dge
+    dge
 }
 
 #' Plot differences between samples: Multidimensional scaling
@@ -75,10 +73,10 @@ edgeR_set_up <- function(counts.df, max.width=10000, lib.size=NULL, keep_a=0.5, 
 #' dge <- edgeR_set_up(counts.df)
 #'
 #' edgeR_plot_mds(dge)
-#dmPlotMDS
+# dmPlotMDS
 edgeR_plot_mds <- function(dge) {
-  group <- dge$samples$group %>% as.character()
-  limma::plotMDS(dge, col = as.numeric(factor(group)))
+    group <- dge$samples$group %>% as.character()
+    limma::plotMDS(dge, col = as.numeric(factor(group)))
 }
 
 
@@ -106,29 +104,33 @@ edgeR_plot_mds <- function(dge) {
 #'
 #' dm_results <- edgeR_results(dge, p.value = 0.01, lfc = 1, regions = example_regions)
 #' head(dm_results)
-#dmResults
-#also need to update this fn - adjusted p val
-edgeR_results <- function(dge, p.value=0.01, lfc=1, regions) {
-  if(!is.numeric(p.value) | length(p.value) > 1) {
-    stop("p.value must be 1 number, recommend using default value")
-  }
-  if(!is.numeric(lfc) | length(lfc) > 1) {
-    stop("lfc must be 1 number, recommend using default value")
-  }
-  if(missing(regions)) {
-    message("GATC region data.frame required")
-  }
-  group <- dge$samples$group %>% as.character()
-  design <- dge$design
-  fit <- edgeR::glmQLFit(dge, design = design)
-  qlf <- edgeR::glmQLFTest(fit, coef = 2)
-  lrt_table <- qlf$table
-  lrt_table <- lrt_table %>% dplyr::mutate(adjust.p = stats::p.adjust(.data$PValue, method = "BH"),
-                                    dm = dplyr::case_when(.data$logFC < lfc & .data$adjust.p < p.value ~ -1,
-                                                   abs(.data$logFC) < lfc ~ 0,
-                                                   .data$logFC > lfc & .data$adjust.p < p.value ~ 1, TRUE ~ 0))
-  lrt_table <- add_de(lrt_table, regions)
-  lrt_table
+# dmResults
+# also need to update this fn - adjusted p val
+edgeR_results <- function(dge, p.value = 0.01, lfc = 1, regions) {
+    if (!is.numeric(p.value) | length(p.value) > 1) {
+        stop("p.value must be 1 number, recommend using default value")
+    }
+    if (!is.numeric(lfc) | length(lfc) > 1) {
+        stop("lfc must be 1 number, recommend using default value")
+    }
+    if (missing(regions)) {
+        message("GATC region data.frame required")
+    }
+    group <- dge$samples$group %>% as.character()
+    design <- dge$design
+    fit <- edgeR::glmQLFit(dge, design = design)
+    qlf <- edgeR::glmQLFTest(fit, coef = 2)
+    lrt_table <- qlf$table
+    lrt_table <- lrt_table %>% dplyr::mutate(
+        adjust.p = stats::p.adjust(.data$PValue, method = "BH"),
+        dm = dplyr::case_when(
+            .data$logFC < lfc & .data$adjust.p < p.value ~ -1,
+            abs(.data$logFC) < lfc ~ 0,
+            .data$logFC > lfc & .data$adjust.p < p.value ~ 1, TRUE ~ 0
+        )
+    )
+    lrt_table <- add_de(lrt_table, regions)
+    lrt_table
 }
 
 
@@ -149,25 +151,29 @@ edgeR_results <- function(dge, p.value=0.01, lfc=1, regions) {
 #' dge <- edgeR_set_up(counts.df)
 #'
 #' edgeR_results_plot(dge, p.value = 0.01, lfc = 1)
-#dmPlotResults
-edgeR_results_plot <- function(dge, p.value=0.01, lfc=1) {
-  if(!is.numeric(p.value) | length(p.value) > 1) {
-    stop("p.value must be 1 number, recommend using default value")
-  }
-  if(!is.numeric(lfc) | length(lfc) > 1) {
-    stop("lfc must be 1 number, recommend using default value")
-  }
-  group <- dge$samples$group %>% as.character()
-  design <- dge$design
-  fit <- edgeR::glmQLFit(dge, design = design)
-  qlf <- edgeR::glmQLFTest(fit, coef = 2)
-  lrt_table <- qlf$table
-  lrt_table <- lrt_table %>% dplyr::mutate(adjust.p = stats::p.adjust(.data$PValue, method = "BH"),
-                                           dm = dplyr::case_when(.data$logFC < -lfc & .data$adjust.p < p.value ~ -1,
-                                                                 abs(.data$logFC) < lfc ~ 0,
-                                                                 .data$logFC > lfc & .data$adjust.p < p.value ~ 1, TRUE ~ 0))
-  detags <- rownames(dge)[as.logical(lrt_table$dm)]
-  edgeR::plotSmear(qlf, de.tags=detags, ylab = "logFC - Fusion/Dam")
+# dmPlotResults
+edgeR_results_plot <- function(dge, p.value = 0.01, lfc = 1) {
+    if (!is.numeric(p.value) | length(p.value) > 1) {
+        stop("p.value must be 1 number, recommend using default value")
+    }
+    if (!is.numeric(lfc) | length(lfc) > 1) {
+        stop("lfc must be 1 number, recommend using default value")
+    }
+    group <- dge$samples$group %>% as.character()
+    design <- dge$design
+    fit <- edgeR::glmQLFit(dge, design = design)
+    qlf <- edgeR::glmQLFTest(fit, coef = 2)
+    lrt_table <- qlf$table
+    lrt_table <- lrt_table %>% dplyr::mutate(
+        adjust.p = stats::p.adjust(.data$PValue, method = "BH"),
+        dm = dplyr::case_when(
+            .data$logFC < -lfc & .data$adjust.p < p.value ~ -1,
+            abs(.data$logFC) < lfc ~ 0,
+            .data$logFC > lfc & .data$adjust.p < p.value ~ 1, TRUE ~ 0
+        )
+    )
+    detags <- rownames(dge)[as.logical(lrt_table$dm)]
+    edgeR::plotSmear(qlf, de.tags = detags, ylab = "logFC - Fusion/Dam")
 }
 
 
@@ -184,27 +190,32 @@ edgeR_results_plot <- function(dge, p.value=0.01, lfc=1) {
 #' * adjust.p: 1 if dm is NA :
 #' * meth_status: Upreg, No_sig, Downreg, Not_included
 add_de <- function(dm_results, regions) {
-  if(!is.data.frame(dm_results)) {
-    stop("Must have data frame of differential testing results from `edgeR_results")
-  }
-  if(!is.data.frame(regions)) {
-    stop("Regions must be a data.frame")
-  }
-  results <- dm_results
-  df <- regions %>%
-    dplyr::mutate(seqnames = paste0("chr", .data$seqnames), number = seq_len(nrow(.)))
-  df$dm <- results[match(df$Position, row.names(results)), "dm"]
-  df$logFC <- results[match(df$Position, row.names(results)), "logFC"]
-  df$PValue <- results[match(df$Position, row.names(results)), "PValue"]
-  df$adjust.p <- results[match(df$Position, row.names(results)), "adjust.p"]
-  df <- df %>%
-    dplyr::mutate(meth_status = dplyr::case_when(is.na(.data$dm) ~ "Not_included",
-                                                 .data$dm == 1 ~ "Upreg",
-                                                 .data$dm == -1 ~ "Downreg",
-                                                 TRUE ~ "No_sig"))
-  df <- df %>%
-    dplyr::mutate(logFC = dplyr::coalesce(.data$logFC, 0),
-                  PValue = dplyr::coalesce(.data$PValue, 1),
-                  adjust.p = dplyr::coalesce(.data$adjust.p, 1))
-  df
+    if (!is.data.frame(dm_results)) {
+        stop("Must have data frame of differential testing results from `edgeR_results")
+    }
+    if (!is.data.frame(regions)) {
+        stop("Regions must be a data.frame")
+    }
+    results <- dm_results
+    df <- regions %>%
+        dplyr::mutate(seqnames = paste0("chr", .data$seqnames), number = seq_len(nrow(.)))
+    df$dm <- results[match(df$Position, row.names(results)), "dm"]
+    df$logFC <- results[match(df$Position, row.names(results)), "logFC"]
+    df$PValue <- results[match(df$Position, row.names(results)), "PValue"]
+    df$adjust.p <- results[match(df$Position, row.names(results)), "adjust.p"]
+    df <- df %>%
+        dplyr::mutate(meth_status = dplyr::case_when(
+            is.na(.data$dm) ~ "Not_included",
+            .data$dm == 1 ~ "Upreg",
+            .data$dm == -1 ~ "Downreg",
+            TRUE ~ "No_sig"
+        ))
+    df <- df %>%
+        dplyr::mutate(
+            logFC = dplyr::coalesce(.data$logFC, 0),
+            PValue = dplyr::coalesce(.data$PValue, 1),
+            adjust.p = dplyr::coalesce(.data$adjust.p, 1)
+        ) %>%
+      data.frame()
+    df
 }
