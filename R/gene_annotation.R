d9b014a9562630b135b198b9ddc8f8ed1bd9561c
Caitlin Page <caitlin.page@petermac.org>
2024-02-29 11:50:35 +1100

    new genes fn and deprecated old get biomart genes

--- R/gene_annotation.R
+++ R/gene_annotation.R
@@ -1,6 +1,7 @@
-#' Extract gene information from Ensembl using biomaRt
+#' Deprecated: Extract gene information from Ensembl using biomaRt
 #'
 #' @description
+#' This function has been deprecated and replaced with `collateGenes`
 #' `get_biomart_genes` accesses the Ensembl database via [biomaRt::useEnsembl() and biomaRt::getBM()] to obtain the location of genes from the selected species.
 #' * Also identifies the number of GATC regions matching to each gene.
 #'
@@ -10,18 +11,10 @@
 #'
 #' @return A data.frame of information about the genes. Columns include: seqnames, start, end, width, strand, ensembl_gene_id, gene_name, ensembl_transcript_id, TSS (transcription start site), n_regions (number of overlapping GATC regions)
 #' @export
-#'
-#' @examples
-#' example_regions <- random_regions()
-#' genes <- get_biomart_genes(
-#'     species = "dmelanogaster_gene_ensembl",
-#'     version = 109,
-#'     regions = example_regions
-#' )
-#' head(genes)
-# getGenes
 get_biomart_genes <- function(species, version = 109, regions) {
-    if (!is.character(species)) {
+  .Deprecated("collateGenes")
+  warning("This function has been deprecated in v0.7.0")
+  if (!is.character(species)) {
         stop("Species must be a character vector")
     }
     if (!is.data.frame(regions)) {
@@ -66,8 +59,99 @@ get_biomart_genes <- function(species, version = 109, regions) {
     gene_features
 }
 
+#' New: Get list of genes
+#'
+#' takes a Txdb object, path to a gff file, or a species (biomart) and returns a GRanges of genes.
+#'
+#' @param genes A Txdb object, path to file, or a species for accessing biomart.
+#' @param regions GATC region file
+#' @param org.Db Required if using a Txdb object so to access gene names
+#' @param version Required for using biomart
+#'
+#' @return GRanges object of genes and available supplementary information - specifically the TSS, and number of GATC regions overlapping the gene.
+#' @export
+#'
+#' @examples
+#' library(TxDb.Dmelanogaster.UCSC.dm6.ensGene)
+#' library(org.Dm.eg.db)
+#' set.seed(123)
+#' example_regions <- random_regions()
+#' txdb <- TxDb.Dmelanogaster.UCSC.dm6.ensGene
+#' genes <- collateGenes(genes=txdb, regions=example_regions, org.Db=org.Dm.eg.db)
+#' head(genes)
+#'
+collateGenes <- function(genes, regions, org.Db=NULL, version=NULL) {
+  regions <- regions
+  if(inherits(genes, "TxDb")) {
+    genes_ <- GenomicFeatures::genes(genes)
+    genes_$ensembl_gene_id <- names(genes_)
+    genes_ <- data.frame(genes_)[,c("seqnames", "start", "end", "width", "strand", "ensembl_gene_id")]
+    gene_names <- AnnotationDbi::select(org.Db, keys = AnnotationDbi::keys(genes), keytype = "ENSEMBL", columns = c("SYMBOL"))
+    genes_$gene_name <- gene_names[match(genes_$ensembl_gene_id, gene_names$ENSEMBL), "SYMBOL"]
+    genes_ <- genes_ %>% dplyr::mutate(TSS = ifelse(.data$strand == "+", .data$start, .data$end))
+    message("TSS taken as start of gene, taking strand into account")
+    genes_ <- plyranges::as_granges(genes_)
+    seq_chr <- GenomeInfoDb::seqlevels(genes_)[1] %>% grepl("chr", .)
+    if(seq_chr == TRUE) {
+      regions$seqnames <- paste0("chr", regions$seqnames)
+    }
+ # } else if (grep("gff", genes)) {
+  #  genes_ <- rtracklayer::import(genes)
+  #  genes_$ensembl_gene_id <- genes_$ID
+  } else if (grep("ensembl", genes)) {
+    genes_ <- access_biomart(species=genes, regions, version)
+  }
+  genes_ <- annotate_gene_gatc(genes_, regions)
+  genes_
+}
 
-#' New: annotation of peaks and genes
+access_biomart <- function(species, regions, version) {
+  if (!requireNamespace("biomaRt", quietly = TRUE)) {
+    stop("Package \"biomaRt\" must be installed to use this function.",
+         call. = FALSE
+    )
+  }
+  ensembl <- biomaRt::useEnsembl(biomart = "genes", dataset = species, version = version)
+  BM.info <- biomaRt::getBM(
+    attributes = c("ensembl_gene_id", "ensembl_transcript_id", "transcript_is_canonical"),
+    filters = "chromosome_name",
+    values = unique(regions$seqnames),
+    mart = ensembl
+  )
+  gene_features <- biomaRt::getBM(
+    attributes = c(
+      "ensembl_gene_id", "external_gene_name", "ensembl_transcript_id",
+      "chromosome_name", "start_position", "end_position", "strand",
+      "transcription_start_site"
+    ),
+    filters = "ensembl_transcript_id",
+    values = dplyr::filter(BM.info, .data$transcript_is_canonical == 1)$ensembl_transcript_id,
+    mart = ensembl
+  )
+  gene_features <- gene_features %>% .[order(.$chromosome_name, .$start_position), ]
+  colnames(gene_features) <- c("ensembl_gene_id", "gene_name", "ensembl_transcript_id", "seqnames", "start", "end", "strand", "TSS")
+  gene_features <- plyranges::as_granges(gene_features)
+}
+
+annotate_gene_gatc <- function(genes, regions) {
+  #regions$seqnames <- paste0("chr", regions$seqnames)
+  overlap <- plyranges::find_overlaps_within(plyranges::as_granges(regions), plyranges::as_granges(genes)) %>%
+    data.frame() %>%
+    dplyr::group_by(.data$ensembl_gene_id) %>%
+    dplyr::summarise(n_regions = dplyr::n()) %>%
+    data.frame()
+  genes_ <- data.frame(genes)
+  genes_$n_regions <- overlap[match(genes_$ensembl_gene_id, overlap$ensembl_gene_id), "n_regions"]
+  genes_ <- genes_ %>%
+    dplyr::mutate(
+      n_regions = dplyr::coalesce(.data$n_regions, 0)
+     # seqnames = ifelse(grepl("chr", seqnames), seqnames, paste0("chr", seqnames))
+    ) %>% .[order(.$start),] %>%.[order(.$seqnames),]
+  genes_ <- plyranges::as_granges(genes_)
+  genes_
+}
+
+#' Annotation of peaks and genes
 #'
 #' @description
 #' `annotate_genes` identifies the closest gene(s) for the peaks outputted from `aggregate_peaks()`.
@@ -87,22 +171,22 @@ get_biomart_genes <- function(species, version = 109, regions) {
 #' * all - all genes matching to each peak and all information
 #' @export
 #' @examples
+#' library(TxDb.Dmelanogaster.UCSC.dm6.ensGene)
+#' library(org.Dm.eg.db)
 #' set.seed(123)
 #' example_regions <- random_regions()
 #' dm_results <- random_edgeR_results()
 #' peaks <- aggregate_peaks(dm_results)
-#'
-#' genes <- get_biomart_genes(
-#'     species = "dmelanogaster_gene_ensembl",
-#'     version = 109,
-#'     regions = example_regions
-#' )
+#' txdb <- TxDb.Dmelanogaster.UCSC.dm6.ensGene
+#' genes <- collateGenes(genes=txdb, regions=example_regions, org.Db=org.Dm.eg.db)
 #'
 #' annotate_genes(peaks, genes, example_regions, max_distance = 5000)
 #' # view all combinations
 #' annotate_genes(peaks, genes, example_regions, max_distance = NULL)
+#'
 annotate_genes <- function(peaks, genes, regions, max_distance = 5000) {
-    if (!is.data.frame(peaks) | !is.data.frame(genes) | !is.data.frame(regions)) {
+    if ((!is.data.frame(peaks) | !is.data.frame(genes) | !is.data.frame(regions)) &&
+        !(inherits(peaks, "GRanges") | inherits(genes, "GRanges") | inherits(regions, "GRanges"))) {
         stop("Require data.frame of peaks as outputted from `aggregate_peaks`, genes, and regions")
     }
     genes_gr <- plyranges::as_granges(genes)
@@ -202,7 +286,8 @@ annotate_stats <- function(combo) {
             start = pmin(.data$gene_start, .data$peak_start),
             end = pmax(.data$gene_end, .data$peak_end),
             width = .data$end - .data$start + 1
-        ) %>%
+        )
+    df <- df %>%
         dplyr::mutate(
             peak_midpoint = (.data$peak_start + .data$peak_end) / 2,
             distance_TSS = .data$TSS - .data$peak_midpoint,
